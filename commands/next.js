cb93ed0b5a33c404cc1c3cb5d6fe2d8252c6e6cf
pablopunk <yo@pablo.pink>
2020-07-20 11:29:45 +0200

    Typescript option (fix #4)

--- commands/next.js
+++ commands/next.js
@@ -1,26 +1,43 @@
 const { spawnSync } = require('child_process')
 const pkg = require('../package.json')
 
-const commadsToExecute = [
-  ['yarn', ['init', '-y']],
-  ['yarn', ['add', 'next', 'react', 'react-dom']],
-  ['yarn', ['add', '--dev', 'typescript', '@types/node', '@types/react']],
-]
+const commadsToExecute = {
+  init: ['yarn', ['init', '-y']],
+  react: ['yarn', ['add', 'next', 'react', 'react-dom']],
+  typescript: [
+    'yarn',
+    ['add', '--dev', 'typescript', '@types/node', '@types/react'],
+  ],
+}
+
+function getCommandsToExecute(args) {
+  const { init, react, typescript } = commadsToExecute
+  const cmds = [init, react]
+
+  if (args.t || args.typescript) {
+    cmds.push(typescript)
+  }
+
+  return cmds
+}
 
 module.exports = (args) => {
   if (args.h || args.help) {
     console.log(`
-    $ ${pkg.name} next [ -h || -help ]
+    $ ${pkg.name} next [ -h || --help || -t || --typescript ]
 
-    Starts a yarn project and adds all dependencies to create a NextJS website with typescript support:
+    Starts a yarn project and adds all dependencies to create
+    aNextJS website with typescript support (if typescript option is passed):
 
-    ${commadsToExecute.map((c) => `${c[0]} ${c[1].join(' ')}`).join('\n    ')}
+    ${Object.values(commadsToExecute)
+      .map((c) => `${c[0]} ${c[1].join(' ')}`)
+      .join('\n    ')}
     `)
 
     return
   }
 
-  for (const command of commadsToExecute) {
+  for (const command of getCommandsToExecute(args)) {
     spawnSync(command[0], command[1], { stdio: 'inherit' })
   }
 }
